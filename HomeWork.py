f63784fab487194d87f0addf5171edbd8ce255d2
ImprovementMajor <71445477+ImprovementMajor@users.noreply.github.com>
2020-09-26 08:19:32 -0400

    initial

---
+++ HomeWork.py
@@ -0,0 +1,3 @@
+print('Please change miles into kilometeres, enter amount of miles: ')
+miles = float(input()) * 1.609
+print (miles)
---
+++ HomeworkNumbers.py
@@ -0,0 +1,5 @@
+a = 5
+b = a - 3
+a = 5 - 2
+print(b)
+print(a)
